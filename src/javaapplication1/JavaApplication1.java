15cb7753000f3e06a1667627b33c2d7162348a89
omkarsinhda <sindhao@sheridancollege.ca>
2023-05-18 17:54:29 -0400

    Revert "second commit"
    This reverts commit c0bc0c3adf390d3f934082e8b3ac106bacb5ae6f.

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -14,9 +14,8 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(" My Hello World");
+        System.out.println("Hello World");
         System.out.println("Second commit");
-        System.out.println("Hi");
     }
     
 }
