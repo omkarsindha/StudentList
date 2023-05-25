3acf2f7d94b237548d4ec1490217443d56088591
omkarsinhda <sindhao@sheridancollege.ca>
2023-05-25 18:58:22 -0400

    new class

---
+++ src/javaapplication1/timepass.java
@@ -0,0 +1,14 @@
+package javaapplication1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Omkar
+ */
+public class timepass {
+    //commit
+}
